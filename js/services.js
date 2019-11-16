8b7cad0616ab781649bb6a0ff19ae3780ce07595
Kaung Khant Zaw <kaungkz93@gmail.com>
2019-11-17 04:57:47 +0630

    added codes for new navbar

--- js/services.js
+++ js/services.js
@@ -3,7 +3,7 @@ const toggle = document.querySelector(".toggle-bar");
 const menuBar = document.querySelector(".menu-bar");
 
 function toggleClicked() {
-  this.classList.toggle("active-toggle");
+  this.classList.toggle("open");
   menuBar.classList.toggle("toggle-active");
 }
 
